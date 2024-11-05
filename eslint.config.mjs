124ed19e82870c8b7305ddc358b98ceadedf8e6e
“leratodosgit config --global user.name Valeriia <valeriatodoseichuk@gmail.com>
2024-11-05 19:34:52 +0200

    refactoring calendar methods

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-
 export default [
-    {files: ["**/*.{js,mjs,cjs,ts}"]},
-    {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-    {languageOptions: { globals: globals.browser }},
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     eslintPluginPrettierRecommended,
-
+    {
+        // Add this configuration
+        rules: {
+            quotes: ["error", "double"], // Enforce double quotes in TypeScript files
+        },
+    },
 ];
